ff323e7751e33b8acc597552caa393c6038852fc
ashpreet710-m <ashpreet710@gmail.com>
2025-11-03 10:35:14 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React + GitHub Actions 🚀</h1>
+      <p>Your CI/CD deployment is working perfectly!</p>
+    </div>
+  );
+}
+
+export default App;
